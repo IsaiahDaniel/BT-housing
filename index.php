ece1218f131aecf33655559504a468155a6c0597
isaiah Daniel <isaiahdaniel203@gmail.com>
2020-10-21 19:00:20 +0100

    finished HTML and CSS

---
+++ index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Property</title>
+</head>
+<body>
+
+  <nav class="navbar navbar-expand-sm navbar-light bg-light fixed-top">
+
+    <div class="container">
+      <a href="#" class="navbar-brand">
+        <img src="images/logo_dark.png" alt="Logo" width="80px">
+      </a>
+      <button class="navbar-toggler" data-toggle="collapse" data-target="#navbar-collapse">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="navbar-collapse collapse" id="navbar-collapse">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a href="#" class="nav-link">About</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">Listings</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="form">
+        <form action="#">
+          <div class="input-group">
+            <input type="text" class="form-control" placeholder="Search">
+            <button type="submit" class="btn btn-outline-dark">Search</button>
+          </div>
+        </form>
+      </div>
+
+    </div>
+  </nav>
+
+  <section id="showcase">
+    <div class="container text-center">
+      <h1 class="display-4 mt-5 text-white mb-4">
+        Looking for a Home, call BT housing Today
+      </h1>
+      <h5 class="text-white mb-4">We make the process seemless......</h5>
+      <a href="#showcase" class="btn btn-primary">Check out Listings</a>
+    </div> 
+  </section>
+
+  <section id="latest-sales">
+    <div class="container">
+      <h2 class="display-3 text-center mb-5 mt-2">Our latest Sales</h2>
+      <div class="row">
+        <div class="col-lg-4 mb-3">
+          <div class="card">
+            <img src="images/homes/home-1.jpg" alt="home" class="card-img-top">
+            <div class="card-body">
+              hello
+            </div>
+          </div>
+        </div>  
+        <div class="col-lg-4">
+          <div class="card">
+            <img src="images/homes/home-2.jpg" alt="home" class="card-img-top">
+            <div class="card-body">
+              hello
+            </div>
+          </div>
+        </div>  
+        <div class="col-lg-4">
+          <div class="card">
+            <img src="images/homes/home-3.jpg" alt="home" class="card-img-top">
+            <div class="card-body">
+              hello
+            </div>
+          </div>
+        </div>  
+        <div class="col-lg-4">
+          <div class="card">
+            <img src="images/homes/home-4.jpg" alt="home" class="card-img-top">
+            <div class="card-body">
+              hello
+            </div>
+          </div>
+        </div>  
+      </div>
+  </section>
+
+  <section id="realtors">
+    <div class="container">
+      <h2 class="display-3 text-center mb-5 mt-2">Our Realtors</h2>
+      <div class="row">
+        <div class="hover col-md-3">
+          <img src="images/realtors/jenny.jpg" alt="">
+        </div>
+        <div class="hover col-md-3">
+          <img src="images/realtors/kyle.jpg" alt="">
+        </div>
+        <div class="hover col-md-3">
+          <img src="images/realtors/mark.jpg" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <footer class="mt-5 text-center text-white">
+    
+    <div id="footer-top">
+      <div class="links">
+        <ul>
+          <li class="nav-item">
+            About Us
+          </li>
+          <li class="nav-item">Faqs</li>
+          <li class="nav-item">Privacy</li>
+          <li class="nav-item">Terms of use</li>
+        </ul>
+      </div>
+
+      
+      <form action="#" class="newsletter">
+        <input type="text" placeholder="Enter Email address" class="form-control mb-2">
+        <input type="submit" value="Subscribe" class="btn btn-outline-light" style="cursor: pointer">
+      </form>
+    
+    </div>
+    
+    <div id="footer-bottom">
+        <p class="pt-4">Copyright &copy; 2020, All rights Reserved</p>
+    </div>
+
+  </footer>
+
+  <script src="js/jquery-3.2.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
